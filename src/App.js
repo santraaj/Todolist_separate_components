772c43b059b5644c986fab11744aea3f822d8116
santraaj <santra.jantunen@gmail.com>
2022-03-17 00:24:01 +0200

    Versio 1, deleteTodo puuttuu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'; 
 import './App.css';
+import TodoTable from './components/TodoTable';
 
-function App() {
+function TodoList() {
+  const [todo, setTodo] = useState({description: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  };
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  };
+  
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index))
+  };
+
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simple Todolist</h1>
       </header>
+      <div class="form">
+        <span>Add todo:</span>
+        <div class="input">
+          <label>Description:</label>
+          <input type="text" onChange={inputChanged} name="description" value={todo.description}/>
+          <label>Date:</label>
+          <input type="text" onChange={inputChanged} name="date" value={todo.date}/>
+          <button onClick={addTodo}>Add</button>
+        </div>
+      </div>
+      <TodoTable todos={todos} />
     </div>
   );
-}
-
-export default App;
+};
+  
+export default TodoList;
---
+++ src/components/TodoTable.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function TodoTable(props) {
+    return(
+        <div>
+            <table>
+          <tbody>
+            <tr><th>Date</th><th>Description</th></tr>
+            {
+              props.todos.map((todo, index) => 
+                <tr key={index}>
+                  <td>{todo.date}</td>
+                  <td>{todo.description}</td>
+                  <td><button id={index} onClick={() => deleteTodo(index)}>Delete</button></td>
+                </tr>)
+            }
+          </tbody>
+        </table>
+        </div>
+    );
+} 
